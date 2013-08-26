284f577a25a97e58beeb777d31553cb4e1d56141
clementpigeon <clementpigeon@gmail.com>
2013-08-26 11:51:19 -0400

    adds populate method

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,8 +2,22 @@ class Minesweeper
 
   def initialize(dimensions, mine_count)
     @dimensions = dimensions
-    @board = Array.new(dimensions) { Array.new(dimensions) { Space.new } }
     @mine_count = mine_count
+    @board = populate_board
+  end
+
+  def populate_board
+    board = Array.new(@dimensions) { Array.new(@dimensions) { Space.new } }
+    placed_mines = 0
+
+    until placed_mines == @mine_count
+      candidate = board.sample.sample
+
+      next if candidate.mine
+      candidate.mine = true
+      placed_mines += 1
+    end
+    board
   end
 
   def print_board
@@ -20,20 +34,21 @@ end
 
 class Space
   attr_accessor :mine, :mine_neighbors, :print
-  #bomb/ empty
-  # bomb_neighbors count
-  # sym =
-  # state = discovered
-  #
   def initialize
     @discovered = false
     @mine = false
     @mine_neighbors = 0
-    @print = "*"
+    @print = get_symbol
   end
 
-  def get_neighbors
+  def get_symbol
+    if @discovered
+
+    else
+      "*"
+    end
 
   end
 
 end
+
