1398d9d9bce0ae92acbe39abf5e771913f47b05f
clementpigeon <clementpigeon@gmail.com>
2013-08-26 14:42:35 -0400

    finished board populate

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,4 +1,6 @@
 class Minesweeper
+  attr_accessor :board
+  attr_reader :dimensions
 
   def initialize(dimensions, mine_count)
     @dimensions = dimensions
@@ -7,17 +9,51 @@ class Minesweeper
   end
 
   def populate_board
-    board = Array.new(@dimensions) { Array.new(@dimensions) { Space.new } }
+    board = Array.new(@dimensions) { Array.new(@dimensions) }
     placed_mines = 0
 
+    #Populate board with mines
     until placed_mines == @mine_count
-      candidate = board.sample.sample
+      row, space = random_coords
 
-      next if candidate.mine
-      candidate.mine = true
+      candidate = board[row][space]
+
+      next if !candidate.nil? && candidate.mine
+      #must reset board here
+      board[row][space] = Space.new([row,space], true, "o")
       placed_mines += 1
     end
+
+    #Populate other spaces
+    board.length.times do |row|
+      board.length.times do |space|
+        board[row][space] = Space.new([row,space], false, "_") unless board[row][space]
+
+      end
+    end
+
+    #Add mine count to spaces
+    board.length.times do |row|
+      board.length.times do |space|
+        #Skip to next space if this is a mine
+        next if board[row][space].mine
+
+        mine_count = 0
+        neighbors = board[row][space].get_neighbors(board, dimensions)
+        neighbors.each do |neighbor|
+          mine_count += 1 if neighbor.mine
+        end
+        puts neighbors.length
+        board[row][space].mine_neighbors = mine_count
+        board[row][space].print = mine_count.to_s unless mine_count == 0
+      end
+    end
+
     board
+  end
+
+  def random_coords
+    [(0...dimensions).to_a.sample, (0...dimensions).to_a.sample]
   end
 
   def print_board
@@ -27,28 +63,61 @@ class Minesweeper
       end
       puts ""
     end
+
     nil
   end
 
 end
 
+class Board
+end
+
 class Space
-  attr_accessor :mine, :mine_neighbors, :print
-  def initialize
-    @discovered = false
-    @mine = false
+  attr_accessor :mine, :mine_neighbors, :print, :discovered, :coords
+
+  def initialize(coords, mine, print)
+    @discovered = true
+    @mine = mine
     @mine_neighbors = 0
-    @print = get_symbol
+    @coords = coords
+    @print = print #get_symbol
   end
 
-  def get_symbol
-    if @discovered
+  def get_neighbors(board, dimensions)
+    neighbors = []
 
-    else
-      "*"
+    neighbors_offset = [
+      [-1, -1], [0, -1], [1, -1],
+      [-1,0], [1,0],
+      [-1, 1], [0, 1], [1, 1]
+    ]
+
+    puts "dimensions: #{dimensions}"
+
+    neighbors_offset.each do |x,y|
+      x_coord, y_coord = (@coords[0] + x), (@coords[1] + y)
+      neighbors << board[x_coord][y_coord] if on_board?([x_coord, y_coord], dimensions)
     end
 
+    neighbors
   end
+
+  def on_board?(coords, dimensions)
+    coords.all? { |num| num.between?(0, dimensions - 1) }
+  end
+
+  # def get_symbol
+#     if discovered
+#       if mine
+#         "o"
+#       else
+#         "_"
+#       end
+#     else
+#       "*"
+#     end
+#
+#   end
 
 end
 
